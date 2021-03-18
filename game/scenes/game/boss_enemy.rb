9124f6e0fbdbd773a6976de50e08bd3b08a57dd5
MechaMacha <y_masaki20@icloud.com>
2021-03-18 22:13:38 +0900

    雑魚敵を実装して、ボスを実装しました。ボスは攻撃を3回当てれば倒せます。

---
+++ game/scenes/game/boss_enemy.rb
@@ -0,0 +1,45 @@
+module Game
+    class Boss_enemy < Sprite
+      @@collection = []
+      @@weapon_check = 0
+  
+      def self.collection
+        @@collection
+      end
+  
+      def self.add(x, y, enemy_img)
+        @@collection << self.new(x, y, enemy_img)
+      end
+  
+      def initialize(x, y, img)
+        self.x = x
+        self.y = y
+        self.image = img
+        @enemy_img = img
+        @enemy_img.set_color_key(C_WHITE) # 指定された画像のC_WHITE（白色）部分を透明化
+        super(self.x,self.y,self.image)
+        @x = x
+        @y = y
+      end
+  
+      def draw
+        #Window.draw(@map.root_x + @x, @map.rooty +@y, @enemy_img, 1)
+        Window.draw(self.x, self.y, @enemy_img,1)
+      end
+  
+      def update
+        #self.x += rand(5) - 3
+        self.x = @x
+        self.y = @y - 20  #rand(5) - 3
+      end
+  
+      def hit(obj)
+
+        @@weapon_check += 1
+        if @@weapon_check == 100
+            puts 1
+            self.class.collection.delete(self)
+        end
+      end
+    end
+  end
